cdf6046bb31270789663224be1eebac3b0c08ee9
silvangemeren <145678672+silvangemeren@users.noreply.github.com>
2024-12-02 15:27:18 +0100

    Delete project-onderzoek directory

--- project-onderzoek/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AboutUsController;
-
-Route::get('/about-us', function() {
-    return view('about-us');
-});
-Route::get('/test', [AboutUsController::class, 'index']);
